1c96a0ad56368973a8f8eb39c7daef42731398c3
pfmao <ErenJagerM@outlook.com>
2021-06-22 20:12:56 +0800

    feat: add server

---
+++ server.js
@@ -0,0 +1,16 @@
+const http = require('http');
+
+// TODO: Read this: https://nodejs.org/en/docs/guides/anatomy-of-an-http-transaction/
+http.createServer((req, res) => {
+    let body = [];
+    req.on('error', (err) => console.log(err))
+        .on('data', (chunk) => body.push(chunk.toString()))
+        .on('end', () => {
+            body = Buffer.concat(body).toString();
+            console.log('body:', body);
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(' Hello World\n');
+        });
+}).listen(8088);
+
+console.log('server started');
