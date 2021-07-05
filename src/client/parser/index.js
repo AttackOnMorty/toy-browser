84c76c5a07bf9118aae2882242d6aa9450996eae
pfmao <ErenJagerM@outlook.com>
2021-07-05 20:59:01 +0800

    feat: parse html attributes

--- src/client/parser/index.js
+++ src/client/parser/index.js
@@ -1,6 +1,7 @@
-// Reference: https://html.spec.whatwg.org/#data-state
+// HTML Tokenization Specification: https://html.spec.whatwg.org/#tokenization
 const EOF = Symbol('EOF');
 let currentToken;
+let currentAttribute;
 
 function parserHTML(html) {
     let state = data;
@@ -14,18 +15,20 @@ function emit(token) {
     console.log(token);
 }
 
+// TODO: Unhandled character: &
 function data(char) {
     if (char === '<') {
         return tagOpen;
-    } else if (char === 'EOF') {
+    } else if (char === '\u0000') {
+    } else if (char === EOF) {
         emit({ type: 'EOF' });
-        return;
     } else {
         emit({ type: 'text', content: char });
         return data;
     }
 }
 
+// TODO: Unhandled characters: !, ?
 function tagOpen(char) {
     if (char === '/') {
         return endTagOpen;
@@ -35,8 +38,8 @@ function tagOpen(char) {
             tagName: '',
         };
         return tagName(char);
+    } else if (char === EOF) {
     } else {
-        return;
     }
 }
 
@@ -48,24 +51,26 @@ function endTagOpen(char) {
         };
         return tagName(char);
     } else if (char === '>') {
-    } else if (char === 'EOF') {
+    } else if (char === EOF) {
     } else {
     }
 }
 
 function tagName(char) {
     if (char.match(/^[\t\n\f ]$/)) {
-        emit(currentToken)
         return beforeAttributeName;
     } else if (char === '/') {
         return selfClosingStartTag;
-    } else if (char.match(/^[a-zA-Z]$/)) {
-        currentToken.tagName += char;
+    } else if (char.match(/^[A-Z]$/)) {
+        currentToken.tagName += char.toLowerCase();
         return tagName;
     } else if (char === '>') {
-        emit(currentToken)
+        emit(currentToken);
         return data;
+    } else if (char === '\u0000') {
+    } else if (char === EOF) {
     } else {
+        currentToken.tagName += char;
         return tagName;
     }
 }
@@ -73,18 +78,146 @@ function tagName(char) {
 function beforeAttributeName(char) {
     if (char.match(/^[\t\n\f ]$/)) {
         return beforeAttributeName;
-    } else if (char === '>') {
-        return data;
+    } else if (char === '/' || char === '>' || char === EOF) {
+        return afterAttributeName(char);
     } else if (char === '=') {
-        return beforeAttributeName;
     } else {
+        currentAttribute = {
+            name: '',
+            value: '',
+        };
+        return attributeName(char);
+    }
+}
+
+function afterAttributeName(char) {
+    if (char.match(/^[\t\n\f ]$/)) {
+        return afterAttributeName;
+    } else if (char === '/') {
+        return selfClosingStartTag;
+    } else if (char === '=') {
+        return beforeAttributeValue;
+    } else if (char === '>') {
+        currentToken[currentAttribute.name] = currentAttribute.value;
+        emit(currentToken);
+        return data;
+    } else if (char === EOF) {
+    } else {
+        currentAttribute = {
+            name: '',
+            value: '',
+        };
+        return attributeName(char);
+    }
+}
+
+function attributeName(char) {
+    if (
+        char.match(/^[\t\n\f ]$/) ||
+        char === '/' ||
+        char === '>' ||
+        char === EOF
+    ) {
+        return afterAttributeName(char);
+    } else if (char === '=') {
+        return beforeAttributeValue;
+    } else if (char.match(/^[A-Z]$/)) {
+        currentToken.tagName += char.toLowerCase();
+        return attributeName;
+    } else if (char === '\u0000') {
+    } else if (char === '"' || char === "'" || char === '<') {
+    } else {
+        currentAttribute.name += char;
+        return attributeName;
+    }
+}
+
+function beforeAttributeValue(char) {
+    if (
+        char.match(/^[\t\n\f ]$/) ||
+        char === '/' ||
+        char === '>' ||
+        char === EOF
+    ) {
+        return beforeAttributeValue;
+    } else if (char === '"') {
+        return doubleQuotedAttributeValue;
+    } else if (char === "'") {
+        return singleQuotedAttributeValue;
+    } else if (char === '>') {
+    } else {
+        return unquotedAttributeValue(char);
+    }
+}
+
+// TODO: Unhandled character: &
+function doubleQuotedAttributeValue(char) {
+    if (char === '"') {
+        currentToken[currentAttribute.name] = currentAttribute.value;
+        return afterQuotedAttributeValue;
+    } else if (char === '\u0000') {
+    } else if (char === EOF) {
+    } else {
+        currentAttribute.value += char;
+        return doubleQuotedAttributeValue;
+    }
+}
+
+// TODO: Unhandled character: &
+function singleQuotedAttributeValue(char) {
+    if (char === "'") {
+        currentToken[currentAttribute.name] = currentAttribute.value;
+        return afterQuotedAttributeValue;
+    } else if (char === '\u0000') {
+    } else if (char === EOF) {
+    } else {
+        currentAttribute.value += char;
+        return singleQuotedAttributeValue;
+    }
+}
+
+// TODO: Unhandled character: &
+function unquotedAttributeValue(char) {
+    if (char.match(/^[\t\n\f ]$/)) {
+        currentToken[currentAttribute.name] = currentAttribute.value;
         return beforeAttributeName;
+    } else if (char === '>') {
+        currentToken[currentAttribute.name] = currentAttribute.value;
+        emit(currentToken);
+        return data;
+    } else if (char === '\u0000') {
+    } else if (
+        char === '"' ||
+        char === "'" ||
+        char === '<' ||
+        char === '=' ||
+        char === '`'
+    ) {
+    } else if (char === EOF) {
+    } else {
+        currentAttribute.value += char;
+        return unquotedAttributeValue;
+    }
+}
+
+function afterQuotedAttributeValue(char) {
+    if (char.match(/^[\t\n\f ]$/)) {
+        return beforeAttributeName;
+    } else if (char === '/') {
+        return selfClosingStartTag;
+    } else if (char === '>') {
+        currentToken[currentAttribute.name] = currentAttribute.value;
+        emit(currentToken);
+        return data;
+    } else if (char === EOF) {
+    } else {
     }
 }
 
 function selfClosingStartTag(char) {
     if (char === '>') {
         currentToken.isSelfClosing = true;
+        emit(currentToken);
         return data;
     } else if (char === 'EOF') {
     } else {
